a659285fc842c2edd0b7e40b717e16a032667b23
woo417 <woo417@nate.com>
2017-08-16 17:44:14 +0900

    serialport update

--- server.js
+++ server.js
@@ -8,5 +8,30 @@
 *  
 */
 var camera = require("./camera/photo"); //사진 촬영 모듈
+var SerialPort = require('serialport');
+var port = new SerialPort('/dev/ttyACM0',{
+baudrate: 9600,
+parser: SerialPort.parsers.readline('\n')
+});
+ 
+port.on('open', function() {
+  port.write('1', function(err) {
+    if (err) {
+      return console.log('Error on write: ', err.message);
+    }
+    console.log('1 written');
+  });
+});
+ 
+// open errors will be emitted as an error event
+port.on('error', function(err) {
+  console.log('Error: ', err.message);
+})
+ 
+port.on('data', function (data) {
+  console.log('Read and Send Data : ' + data);
+  port.write(data);
+});
+
 
 camera.start(); //카메라 모듈 실행
