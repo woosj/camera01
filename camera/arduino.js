6f7a300f0e42371e76e5af8668a06af08c3dc72b
woo417 <woo417@nate.com>
2017-08-17 11:01:20 +0900

    serial module change

--- camera/arduino.js
+++ camera/arduino.js
@@ -1,10 +1,22 @@
 ﻿
-var SerialPort = require('serialport'); //아두이노와 시리얼 통신할 수 있는 모듈
+var five = require("johnny-five");
+
+board.on("ready", function () {
+
+    var analog = new five.Pin("A0");
+
+    // Query the analog pin for its current state.
+    analog.query(function (state) {
+        console.log(state);
+    });
+});
+
+/*var SerialPort = require('serialport'); //아두이노와 시리얼 통신할 수 있는 모듈
 
 //라즈베리파이와 연결된 디바이스 주소
 var port = new SerialPort('/dev/ttyACM0', {
     baudrate: 9600,
-    parser: SerialPort.parsers.readline("\n")
+    parser: SerialPort.parsers.readline('\n')
 });
 
 //포트 열기
@@ -24,3 +36,4 @@ port.on('data', function (data) {
 });
 
 module.exports = port;
+*/
