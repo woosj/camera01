dcc05e94975da2a48ff2e44c3f1a55b52f56ee7c
woo417 <woo417@nate.com>
2017-08-24 16:32:59 +0900

    arduino

--- camera/arduino.js
+++ camera/arduino.js
@@ -1,5 +1,5 @@
 ﻿
-var SerialPort = require('serialport'); //아두이노와 시리얼 통신할 수 있는 모듈
+/*var SerialPort = require('serialport'); //아두이노와 시리얼 통신할 수 있는 모듈
 var http = require('http');
 var options = {
     host: '127.0.0.1',
@@ -50,5 +50,26 @@ port.on('data', function (data) {
     //req.end();
     //port.write(data);
 });
+*/
+
+
+const SerialPort = require('serialport');
+const parsers = SerialPort.parsers;
+
+// Use a `\r\n` as a line terminator
+const parser = new parsers.Readline({
+    delimiter: '\r\n'
+});
+
+const port = new SerialPort('/dev/ttyACM0', {
+    baudRate: 9600
+});
+
+port.pipe(parser);
+
+port.on('open', () => console.log('Port open'));
+
+parser.on('data', console.log);
+
 
 module.exports = port;
