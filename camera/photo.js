f7f8eced78fe8f0a84e5b23d61d2ae481ce23b40
woo417 <woo417@nate.com>
2017-08-24 15:04:25 +0900

    add date

--- camera/photo.js
+++ camera/photo.js
@@ -22,7 +22,7 @@ var camera = new RaspiCam({
     encoding: 'jpg',
     output: "./images/" + moment().format('YYYYMMDDHHmmss') + ".jpg",//"images/image_%06d.jpg", // image_000001.jpg, image_000002.jpg,... 
     q: 50,
-    timeout: 0, // take a total of 4 pictures over 12 seconds , 0 일경우 무제한 촬영
+    timeout: 1000, // take a total of 4 pictures over 12 seconds , 0 일경우 무제한 촬영
     timelapse: 1000, // take a picture every 1 hours
     nopreview: true,
     th: '0:0:0'
@@ -69,6 +69,9 @@ camera.on("read", function (err, timestamp, filename) {
 //모듈 종료
 camera.on("exit", function (timestamp) {
     console.log("timelapse child process has exited");
+    camera.stop();
+    console.log('Restarting camera...')
+    camera.start()
 });
 
 //모듈 정지
