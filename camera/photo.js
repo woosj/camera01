60bc69e303d0fe50b961209e62b133aa8c0dda7b
woo417 <woo417@nate.com>
2017-08-25 11:12:13 +0900

    test

--- camera/photo.js
+++ camera/photo.js
@@ -22,7 +22,6 @@ var option = {
     encoding: 'jpg',
     output: "./images/" + moment().format('YYYYMMDDHHmmss') + ".jpg",//"images/image_%06d.jpg", // image_000001.jpg, image_000002.jpg,... 
     q: 50,
-    timeout: 10000, // take a total of 4 pictures over 12 seconds , 0 일경우 무제한 촬영
     nopreview: true,
     th: '0:0:0'
 };
