a76965e62d02f89b106cc9932991489d7a12d20b
woo417 <woo417@nate.com>
2017-08-25 15:52:13 +0900

    test

--- camera/photo2.js
+++ camera/photo2.js
@@ -60,7 +60,7 @@ exports.takeImage = function () {
         '-o', path_name   // path + name
     ];
     process = spawn('raspistill', args);
-    process.on('exit', this.sendImage(path_name));
+    process.on('exit', this.sendImage, path_name);
 }
 
 exports.sendImage = function (path_name) {
