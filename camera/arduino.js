bbc9140b05add4bea5a978905e14b42584a66a26
woo417 <woo417@nate.com>
2017-08-29 12:22:47 +0900

    mqtt

--- camera/arduino.js
+++ camera/arduino.js
@@ -21,12 +21,14 @@ port.pipe(parser);
 //MQTT pub/sub
 client.on('connect', function () {
     client.subscribe('/1/onoff');
+    client.publish('/1/onoff',"on");
 })
 
 //callback
 client.on('message', function (topic, message) {
     // message is Buffer 
     console.log(message.toString());
+    serial.write(message.toString(), function (err) { });
     client.end();
 })
 
